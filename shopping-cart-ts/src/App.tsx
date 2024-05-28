cb09fb9e1d36d04c1c4dc1cc15d3d3bdf93ee88e
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-05-27 21:59:43 -0600

    add shopping cart with typescript initial files

---
+++ shopping-cart-ts/src/App.tsx
@@ -0,0 +1,48 @@
+import Header from './components/Header';
+import Guitar from './components/Guitar';
+import useCart from './hooks/useCart';
+function App() {
+
+  
+  const  {data, cart, addToCart, removeFromCart, increment, decrement, clearCart, isEmpty, cartTotal} = useCart();
+
+  return (
+    <>
+    
+    <Header
+      cart={cart}
+      removeFromCart={removeFromCart}
+      increment={increment}
+      decrement={decrement}
+      clearCart={clearCart}
+      isEmpty={isEmpty}
+      cartTotal={cartTotal}
+    />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+        {data.map((guitar)=>(
+          <Guitar
+          guitar={guitar}
+          key={guitar.id}
+          addToCart={addToCart}
+          />
+        ))}
+
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
