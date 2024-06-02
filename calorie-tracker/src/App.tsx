397fd43177242f1d9f27592b86172c1da05005c2
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-06-02 16:31:33 -0600

    chore: Update dependencies and add uuid package for unique identifiers

--- calorie-tracker/src/App.tsx
+++ calorie-tracker/src/App.tsx
@@ -1,8 +1,26 @@
-
+import Form from "./components/Form"
+import { useReducer } from "react"
+import { activityReducer, initialState } from "./reducers/activity-reducers"
 function App() {
+
+  const [state, dispatch] = useReducer(activityReducer, initialState)
+  
   return (
     <>
-      <h1 className="bg-blue-500">Calorie Tracker</h1>
+      <header className="bg-sky-500 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Calorie Tracker
+          </h1>
+        </div>
+      </header>
+      <section className="bg-sky-300 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+        <Form 
+          dispatch={dispatch}
+        />
+        </div>
+      </section>
     </>
   )
 }
