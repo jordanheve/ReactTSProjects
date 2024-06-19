296b009d3d8dc1bb9472ba8d19ad2a3406feaa4c
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-06-19 07:48:51 -0600

    chore: Update App layout and add BudgetForm component

---
+++ budget-manager/src/components/BudgetForm.tsx
@@ -0,0 +1,48 @@
+import { useState, useMemo } from "react";
+import { ChangeEvent } from "react";
+export default function BudgetForm() {
+    const [budget, setBudget] = useState<number | null>(0);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      let string = e.target.value;
+      string = string.replace(/^0+/, "").replace(/\D/g, "");
+      const number = parseInt(string);
+      if (isNaN(number)) {
+        setBudget(null);
+      } else {
+        setBudget(number);
+      }
+    };
+  
+    const isValid = useMemo(() => {
+      return budget === null || budget <= 0;
+    }, [budget]);
+
+  return (
+    <form action="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Budget
+        </label>
+        <input
+          type="text"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Enter your budget"
+          name="budget"
+          id="budget"
+          onChange={handleChange}
+          value={budget !== null ? budget : ""}
+        />
+      </div>
+      <input
+        type="button"
+        value="Submit"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white uppercase font-bold text-center  disabled:cursor-not-allowed disabled:opacity-50"
+        disabled={isValid}
+      />
+    </form>
+  );
+}
