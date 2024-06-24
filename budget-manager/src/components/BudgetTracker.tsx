d9980ade5e13a9b82846a7b7ddc7548571f5ab76
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-06-24 07:07:48 -0600

    chore: Update BudgetTracker component layout and add image and buttons

--- budget-manager/src/components/BudgetTracker.tsx
+++ budget-manager/src/components/BudgetTracker.tsx
@@ -1,6 +1,34 @@
+import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
   return (
-    <div>BudgetTracker</div>
+    <div className="grid grid-cols-1 md:grid-cols2 gap-5">
+      <div className="flex justify-center">
+        <img src="/grafico.jpg" alt="grafica de datos" />
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-8">
+            <button
+            type="button"
+            className="bg-pink-600 text-white font-bold py-2 px-4 rounded"
+            onClick={() => console.log("hellor there")}
+            >
+              Reset
+            </button>
+
+            <AmountDisplay 
+              label='Budget'
+              amount={1000}
+            />
+             <AmountDisplay 
+              label='Available'
+              amount={500}
+            />
+             <AmountDisplay 
+              label='Expenses'
+              amount={500}
+            />
+      </div>
+    </div>
   )
 }
