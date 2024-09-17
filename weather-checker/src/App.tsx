1baac9426e69b07997bbaee94517dc51d4a83f0a
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-09-17 08:06:51 -0600

    chore: Add Vite configuration files and update project structure for weather-checker app

---
+++ weather-checker/src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css'
+function App() {
+
+  return (
+    <>
+     <h1 className={styles.title}>clima</h1>
+     <div className={styles.container}>
+
+     </div>
+    </>
+  )
+}
+
+export default App
