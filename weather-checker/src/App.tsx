d674dbe75b8a198bc3c23bb419bb1aca25271bcc
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-09-21 19:11:03 -0600

    refactor: Update App component and add Form component
    - Refactor the App component to import the Form component and render it within the container div.
    - Update the import statement for the App module CSS file.
    - Add the Form component to the App component's JSX.
    - Update the CSS classes in the App module CSS file.

--- weather-checker/src/App.tsx
+++ weather-checker/src/App.tsx
@@ -1,14 +1,14 @@
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import Form from "./components/form/Form";
 function App() {
-
   return (
     <>
-     <h1 className={styles.title}>clima</h1>
-     <div className={styles.container}>
-
-     </div>
+      <h1 className={styles.title}>clima</h1>
+      <div className={styles.container}>
+        <Form />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ weather-checker/src/components/alert/Alert.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './Alert.module.css'
+export default function Alert({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.alert}>{children}</div>
+  )
+}
