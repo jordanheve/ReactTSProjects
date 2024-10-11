2e828519aa04525f890da6450ac680664b11cde1
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-10-11 08:02:21 -0600

    refactor: Update crypto-schema and store.ts files
    - Add CryptoCurrencyResponseSchema to crypto-schema.ts
    - Modify store.ts to fetch and parse crypto data using CryptoCurrenciesSchema

--- crypto-tracker/src/schema/crypto-schema.ts
+++ crypto-tracker/src/schema/crypto-schema.ts
@@ -4,3 +4,12 @@ export const CurrencySchema = z.object({
     code: z.string(),
     name: z.string(),
 });
+
+export const CryptoCurrencyResponseSchema = z.object({
+    CoinInfo: z.object({
+        Fullname: z.string(),
+        Name: z.string(),
+    }),
+});
+
+export const CryptoCurrenciesSchema = z.array(CryptoCurrencyResponseSchema);
--- crypto-tracker/src/store.ts
+++ crypto-tracker/src/store.ts
@@ -1,14 +1,27 @@
 import { create } from 'zustand';
 import axios from 'axios';
+import { CryptoCurrenciesSchema } from './schema/crypto-schema';
+import { CryptoCurrency } from './types';
 
-async function getCryptops() {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    const {data: {Data}} = await axios(url)
-    console.log(Data)
+
+type CryptoStore = {
+    cryptoCurrencies: CryptoCurrency[]
+    fetchCryptos: () => Promise<void>
 }
 
-export const useCryptoStore = create(() => ({
-    fetchCryptos: () => {
-        console.log('fetching cryptos')
+async function getCryptos() {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+    const {data: {Data}} = await axios(url)
+    const result = CryptoCurrenciesSchema.safeParse(Data)
+    if (result.success) {
+        return result.data
+    }
+}
+
+export const useCryptoStore = create<CryptoStore>((set) => ({
+    cryptoCurrencies: [],
+    fetchCryptos: async () => {
+        const cryptoCurrencies = await getCryptos()
+        set(() => ({cryptoCurrencies}))
     }
 }))
